2f4847f67d4def81202316fd127194224d6ca9e4
Dimitri Leurs <dimitri.leurs@numberly.com>
2020-12-24 11:00:31 +0100

    Counter App working

--- app/src/main/java/fr/dleurs/android/counterapp/MainActivity.kt
+++ app/src/main/java/fr/dleurs/android/counterapp/MainActivity.kt
@@ -2,10 +2,34 @@ package fr.dleurs.android.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var counter:Int = 0
+
+    fun setTextValue() {
+        val value = findViewById<TextView>(R.id.value)
+        value.setText(counter.toString())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTextValue()
+
+        val plusBtn = findViewById<Button>(R.id.plusBtn)
+        plusBtn.setOnClickListener {
+            counter++
+            setTextValue()
+        }
+
+        val minusBtn = findViewById<Button>(R.id.minusBtn)
+        minusBtn.setOnClickListener {
+            counter--
+            setTextValue()
+        }
     }
 }
